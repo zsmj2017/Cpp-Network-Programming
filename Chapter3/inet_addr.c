cc6b52f9c42c7823b516610cd03ba07e528f918a
liuxiang <sphinxs@vip.qq.com>
2020-07-10 15:05:48 +0800

    add inet_addr.c

---
+++ Chapter3/inet_addr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+
+int main(int argc, char * argv[]) {
+	char * addr1 = "1.2.3.4";
+	char * addr2 = "1.2.3.256";
+
+	unsigned long conv_addr = inet_addr(addr1);
+	if (conv_addr == INADDR_NONE) {
+		printf("Here is a error in convert!\n");
+	} else {
+		printf("Network ordered addr: %#lx\n", conv_addr);
+	}
+	
+	conv_addr = inet_addr(addr2);
+	if (conv_addr == INADDR_NONE) {
+		printf("Here is a error in convert!\n");
+	} else {
+		printf("Network ordered addr: %#lx\n", conv_addr);
+	}
+
+	return 0;
+}
+
