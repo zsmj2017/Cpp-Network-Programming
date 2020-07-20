05c10e4bfa6531c6d794fa91d1900f3ca37df323
liuxiang <sphinxs@vip.qq.com>
2020-07-20 15:45:18 +0800

    Refine code with google style

--- Chapter2/tcp_client.c
+++ Chapter2/tcp_client.c
@@ -1,55 +1,55 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 void error_handling(const char* message);
 
 int main(int argc, char* argv[]) {
-	int sock;
-	struct sockaddr_in serv_addr;
-	char message[30];
-	int str_len = 0;
-	int idx = 0, read_len = 0;
+    int sock;
+    struct sockaddr_in serv_addr;
+    char message[30];
+    int str_len = 0;
+    int idx = 0, read_len = 0;
 
-	if (argc != 3) {
-		printf("Usage : %s <IP> <port>\n", argv[0]);
-		exit(1);
-	}
+    if (argc != 3) {
+        printf("Usage : %s <IP> <port>\n", argv[0]);
+        exit(1);
+    }
 
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock == -1) {
-		error_handling("socket() error");
-	}
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        error_handling("socket() error");
+    }
 
-	memset(&serv_addr, 0 ,sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
 
-	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
-		error_handling("connect() error");
-	}
-	
-	// 每一次只读取一个字符
-	// 结果与批量读取无异，表明tcp在传输数据时没有数据边界
-	while(read_len = read(sock, &message[idx++], 1)) {
-		if (str_len == -1) {
-			error_handling("read() error");
-		}
-		str_len += read_len;
-		printf("Receive %dth Byte\n", idx);
-	}
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+        error_handling("connect() error");
+    }
 
-	printf("Message from server : %s \n", message);
-	close(sock);
-	return 0;
+    // 每一次只读取一个字符
+    // 结果与批量读取无异，表明tcp在传输数据时没有数据边界
+    while (read_len = read(sock, &message[idx++], 1)) {
+        if (str_len == -1) {
+            error_handling("read() error");
+        }
+        str_len += read_len;
+        printf("Receive %dth Byte\n", idx);
+    }
+
+    printf("Message from server : %s \n", message);
+    close(sock);
+    return 0;
 }
 
 void error_handling(const char* message) {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
--- Chapter3/inet_addr.c
+++ Chapter3/inet_addr.c
@@ -1,24 +1,23 @@
-#include <stdio.h>
 #include <arpa/inet.h>
+#include <stdio.h>
 
-int main(int argc, char * argv[]) {
-	char * addr1 = "1.2.3.4";
-	char * addr2 = "1.2.3.256";
+int main(int argc, char* argv[]) {
+    char* addr1 = "1.2.3.4";
+    char* addr2 = "1.2.3.256";
 
-	unsigned long conv_addr = inet_addr(addr1);
-	if (conv_addr == INADDR_NONE) {
-		printf("Here is a error in convert!\n");
-	} else {
-		printf("Network ordered addr: %#lx\n", conv_addr);
-	}
-	
-	conv_addr = inet_addr(addr2);
-	if (conv_addr == INADDR_NONE) {
-		printf("Here is a error in convert!\n");
-	} else {
-		printf("Network ordered addr: %#lx\n", conv_addr);
-	}
+    unsigned long conv_addr = inet_addr(addr1);
+    if (conv_addr == INADDR_NONE) {
+        printf("Here is a error in convert!\n");
+    } else {
+        printf("Network ordered addr: %#lx\n", conv_addr);
+    }
 
-	return 0;
+    conv_addr = inet_addr(addr2);
+    if (conv_addr == INADDR_NONE) {
+        printf("Here is a error in convert!\n");
+    } else {
+        printf("Network ordered addr: %#lx\n", conv_addr);
+    }
+
+    return 0;
 }
-
